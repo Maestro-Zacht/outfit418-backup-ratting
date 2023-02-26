f7f1a835d8caadca855d170731d00fa148f9576b
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-02-26 12:04:48 +0100

    ignore empty entries and rotations

--- outfit418_backup_ratting/tasks.py
+++ outfit418_backup_ratting/tasks.py
@@ -45,47 +45,51 @@ def save_import(data):
         )
 
         for entry_data in rotation_data['entries']:
-            creator = get_user_or_fake(entry_data['created_by'])
-            char = get_or_create_char(entry_data['created_by'])
 
-            entry = rotation.entries.create(
-                estimated_total=entry_data['estimated_total'],
-                created_by=creator,
-            )
+            if len(entry_data['shares']) > 0:
+                creator = get_user_or_fake(entry_data['created_by'])
+                char = get_or_create_char(entry_data['created_by'])
 
-            if creator == fake_user:
-                EntryCreator.objects.create(
-                    entry=entry,
-                    creator_character=char
+                entry = rotation.entries.create(
+                    estimated_total=entry_data['estimated_total'],
+                    created_by=creator,
                 )
 
-            Entry.objects.filter(pk=entry.pk).update(
-                created_at=entry_data['created_at'],
-                updated_at=entry_data['updated_at'],
-            )
-
-            role = entry.roles.create(
-                name='Krab',
-                value=1,
-            )
-
-            for share_data in entry_data['shares']:
-                user = get_user_or_fake(share_data['character'])
-                character = get_or_create_char(share_data['character'])
-
-                share = entry.ratting_shares.create(
-                    user=user,
-                    user_character=character,
-                    role=role,
-                    site_count=share_data['share_count'],
-                    helped_setup=share_data['helped_setup'],
-                )
-
-                if user == fake_user:
-                    ShareUser.objects.create(
-                        share=share,
-                        character=character
+                if creator == fake_user:
+                    EntryCreator.objects.create(
+                        entry=entry,
+                        creator_character=char
                     )
+
+                Entry.objects.filter(pk=entry.pk).update(
+                    created_at=entry_data['created_at'],
+                    updated_at=entry_data['updated_at'],
+                )
+
+                role = entry.roles.create(
+                    name='Krab',
+                    value=1,
+                )
+
+                for share_data in entry_data['shares']:
+                    user = get_user_or_fake(share_data['character'])
+                    character = get_or_create_char(share_data['character'])
+
+                    share = entry.ratting_shares.create(
+                        user=user,
+                        user_character=character,
+                        role=role,
+                        site_count=share_data['share_count'],
+                        helped_setup=share_data['helped_setup'],
+                    )
+
+                    if user == fake_user:
+                        ShareUser.objects.create(
+                            share=share,
+                            character=character
+                        )
+        if rotation.entries.count() == 0:
+            rotation.delete()
 
 
 @shared_task
